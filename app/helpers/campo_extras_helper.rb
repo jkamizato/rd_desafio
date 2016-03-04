4f61a28339d31cd9410414214b90a37f3bb9c534
Júlio Kamizato <jkamizato@gmail.com>
2016-03-04 18:41:49 -0300

    Codigo mais limpo

--- app/helpers/campo_extras_helper.rb
+++ app/helpers/campo_extras_helper.rb
@@ -1,2 +1,15 @@
 module CampoExtrasHelper
+
+  def build_campo_extra(campo, valor_extra)
+    case campo.campo_tipo when 'text'
+      text_field_tag "campo_extra[#{campo.id}][#{campo.chave.to_sym}]", valor_extra[campo.id], class: 'form-control'
+    when 'textarea'
+        text_area_tag "campo_extra[#{campo.id}][#{campo.chave.to_sym}]", valor_extra[campo.id], class: 'form-control'
+    when 'combobox'
+      @option = eval(campo.select_option)
+      select_tag "campo_extra[#{campo.id}][#{campo.chave.to_sym}]",
+                       options_for_select(@option, valor_extra[campo.id]), class: 'form-control'
+
+    end
+  end
 end
