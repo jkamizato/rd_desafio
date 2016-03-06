1108138f28af0a05260fab43028ad14e1b3816e1
Júlio Kamizato <jkamizato@gmail.com>
2016-03-06 11:51:43 -0300

    Adicionei testes de view

--- spec/factories/campo_extras.rb
+++ spec/factories/campo_extras.rb
@@ -1,5 +1,26 @@
 FactoryGirl.define do
-  factory :campo_extra do
-    
+  factory :campo_extra, class: CampoExtra do |f|
+    f.label 'Idade'
+    f.chave 'idade'
+    f.campo_tipo 'text'
+  end
+
+  factory :campo_extra_text, class: CampoExtra do |f|
+    f.label 'Text'
+    f.chave 'text'
+    f.campo_tipo 'text'
+  end
+
+  factory :campo_extra_textarea, class: CampoExtra do |f|
+    f.label 'Textarea'
+    f.chave 'textarea'
+    f.campo_tipo 'textarea'
+  end
+
+  factory :campo_extra_combobox, class: CampoExtra do |f|
+    f.label 'Combobox'
+    f.chave  'combobox'
+    f.campo_tipo 'combobox'
+    f.select_option "[['Text1', 'item1'], ['Text2', 'item2']]"
   end
 end
--- spec/views/contatos/new.html.erb_spec.rb
+++ spec/views/contatos/new.html.erb_spec.rb
@@ -2,17 +2,47 @@ require 'rails_helper'
 
 RSpec.describe "contatos/new", type: :view do
   include Devise::TestHelpers
+  include FactoryGirl::Syntax::Methods
+
 
   before do
-    user = FactoryGirl.create(:user)
-    sign_in user
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+    @campo_extra = Array.new
+    @campo_extra << FactoryGirl.create(:campo_extra)
+    @valor_extra = Hash.new
   end
 
   it 'must have email field' do
+
     assign(:contato, Contato.new)
-    #render
-    # rendered.should contain('Email')
+    assign(:campo_extra, @campo_extra)
+    render
+    expect(rendered).to match /Email/
   end
 
-  # it 'when have campo extra texto, must render it'
+  it 'when have campo extra texto, must render it' do
+    @campo_extra << FactoryGirl.create(:campo_extra_text)
+    assign(:contato, Contato.new)
+    assign(:campo_extra, @campo_extra)
+    render
+    expect(rendered).to match /Text/
+  end
+
+  it 'when have campo extra textarea, must render it' do
+    @campo_extra << FactoryGirl.create(:campo_extra_textarea)
+
+    assign(:contato, Contato.new)
+    assign(:campo_extra, @campo_extra)
+    render
+    expect(rendered).to match /Textarea/
+  end
+
+  it 'when have combobox, must render it' do
+    @campo_extra << FactoryGirl.create(:campo_extra_combobox)
+    assign(:contato, Contato.new)
+    assign(:campo_extra, @campo_extra)
+    render
+    expect(rendered).to match /Combobox/
+  end
 end
